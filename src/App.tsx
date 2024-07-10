a8befa0f10b014128c01e941a77a0bc573781686
Vindyani1999 <k.a.c.h.vindyani@gmail.com>
2024-07-10 22:06:44 +0530

    trying test file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Hello from "./test";
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Hello name="Frontend" />
     </div>
   );
 }

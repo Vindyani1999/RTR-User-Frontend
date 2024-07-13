9c071ea92d93ef9f9212f4445d1d972c6dc278ad
Vindyani1999 <k.a.c.h.vindyani@gmail.com>
2024-07-13 20:11:04 +0530

    Setup frontend with MUI

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#DC5F00",
+    },
+    secondary: {
+      main: "#373A40",
+    },
+  },
+});
+
+export default theme;
